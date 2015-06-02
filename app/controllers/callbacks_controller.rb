c3faabd185c276c39d3ad22cdef380f9cc52fede
fanfilmu <fanflm@gmail.com>
2015-06-03 00:54:20 +0200

    Change path on failed login

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -3,4 +3,10 @@ class CallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth request.env["omniauth.auth"]
     sign_in_and_redirect @user
   end
+
+  private
+
+  def after_omniauth_failure_path_for(*)
+    root_path
+  end
 end
