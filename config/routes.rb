c245483187cfbc4913994b0bd8de3126470a7867
fanfilmu <fanflm@gmail.com>
2015-06-02 20:39:38 +0200

    Add basic layout styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "callbacks" }
+
+  devise_scope :user do
+    get "sign_out", :to => "devise/sessions#destroy", :as => :destroy_user_session
+  end
 end
