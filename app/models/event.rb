89204d3a0d76fee5513a9051fda9a006c821399e
Bartosz Zurkowski <b.zurkowski93@gmail.com>
2015-06-13 19:04:36 +0200

    Add missing event validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   has_many :comments, as: :commentable
+
+  validates :title, :description, presence: true
+  validates :description, length: { maximum: 1000 }
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -7,6 +7,14 @@ describe Event do
     end
   end
 
+  describe "validations" do
+    %i(title description).each do |field|
+      it { should validate_presence_of field }
+    end
+
+    it { should validate_length_of(:description).is_at_most 1000 }
+  end
+
   describe "associations" do
     it { should have_many :comments }
   end
