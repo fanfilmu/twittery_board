a2e3abb424e0f1a77377902313991455efefae89
fanfilmu <fanflm@gmail.com>
2015-06-14 20:49:28 +0200

    Add User#twitter spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,24 @@ describe User do
   end
 
   it_behaves_like "omniauthable"
+
+  describe "#twitter" do
+    subject { described_class.new token: "ABC", secret: "123" }
+    let(:twitter_client) { double }
+
+    before do
+      allow(Twitter::REST::Client).to receive(:new).and_yield twitter_client
+      ENV["TWITTER_API_KEY"]    = "API-123"
+      ENV["TWITTER_APP_SECRET"] = "SEC-AAA"
+    end
+
+    it "assigns proper keys to client" do
+      expect(twitter_client).to receive(:consumer_key=).with("API-123").and_return true
+      expect(twitter_client).to receive(:consumer_secret=).with("SEC-AAA").and_return true
+      expect(twitter_client).to receive(:access_token=).with("ABC").and_return true
+      expect(twitter_client).to receive(:access_token_secret=).with("123").and_return true
+
+      subject.twitter
+    end
+  end
 end
